4239acc34eb37bca8e2414f3ab839bccc359f503
Erwin Nahue <erwinnahue@gmail.com>
2023-02-21 22:39:27 -0500

    Add navigate return to home on logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,41 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { setNameTrainerGlobal } from '../store/slices/nameTrainer.slice'
-import "./styles/Home.css"
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setNameTrainerGlobal } from "../store/slices/nameTrainer.slice";
+import "./styles/Home.css";
 
 const Home = () => {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const nameTrainer = e.target.nameTrainer.value
-    dispatch(setNameTrainerGlobal(nameTrainer))
-  }
+    e.preventDefault();
+    const nameTrainer = e.target.nameTrainer.value;
+    dispatch(setNameTrainerGlobal(nameTrainer));
+  };
 
   return (
-    <main className='home-block'>
-      <section className='home-section'>
-        <div className='container-home-img'>
-        <div className='home-img'>
-          <img src="/images/pokedex.png" alt="" />
+    <main className="home-block">
+      <section className="home-section">
+        <div className="container-home-img">
+          <div className="home-img">
+            <img src="/images/pokedex.png" alt="" />
+          </div>
         </div>
-        </div>
-        <div>
-        <h2>Hello trainer!</h2>
-        <p>Give me your name to start!</p>
-        <form onSubmit={handleSubmit}>
-          <input id='nameTrainer' type="text" placeholder='your name...' required />
-          <button>Start</button>
-        </form>
+        <div className="home-info">
+          <h2>Hello trainer!</h2>
+          <p>Give me your name to start!</p>
+          <form onSubmit={handleSubmit} className="home-start">
+            <input
+              id="nameTrainer"
+              type="text"
+              placeholder="your name..."
+              required
+            />
+            <button><i class='bx bxl-telegram'></i></button>
+          </form>
         </div>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
